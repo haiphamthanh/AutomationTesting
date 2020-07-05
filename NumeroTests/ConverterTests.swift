26ea9149b2f041e60b2d47f4cde6d34c0f9970f3
PhamThanhHai <pthai076@gmail.com>
2020-07-06 00:10:30 +0700

    [Update] testcase for jenkins

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -10,21 +10,26 @@ import XCTest
 @testable import Numero
 
 class ConverterTests: XCTestCase {
-  let converter = Converter()
-  
-  override func setUp() {
-    super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-  }
-  
-  override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    super.tearDown()
-  }
-  
-  func testConversionForOne() {
-    let result = converter.convert(1)
+    let converter = Converter()
     
-    XCTAssertEqual(result, "", "Conversion for 1 is incorrect")
-  }
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testConversionForOne() {
+        let result = converter.convert(1)
+        
+        XCTAssertEqual(result, "", "Conversion for 1 is incorrect")
+    }
+    
+    func testConversionForTwo() {
+        let result = converter.convert(2)
+        XCTAssertEqual(result, "II", "Conversion for 2 is incorrect")
+    }
 }
